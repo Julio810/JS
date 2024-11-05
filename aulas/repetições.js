64c919394bd46c0ed2e00fb121cb3f377e29a81b
Julio <erison.julio@hotmail.com>
2024-11-05 12:17:18 -0300

    Repetições

---
+++ aulas/repetições.js
@@ -0,0 +1,17 @@
+/*var c = 1
+while(c <= 10){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 2)*/
+
+console.log('Vai começar...')
+for (let c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM')
